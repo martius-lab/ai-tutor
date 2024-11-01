0ce654e7346ac09816e2e17f9ecf7b88fccd84de
Marius Kinzelmann <marius.kinzelmann@gmail.com>
2024-11-01 15:14:10 +0100

    Created base components for the files in /pages and added them to the app pages in the basic app with respective routing. Also edited __init__.py for easier importing of pages and components.

---
+++ teamproject_ai_tandem_partner/pages/__init__.py
@@ -0,0 +1,13 @@
+from .login import login_default
+from .settings import settings_default
+from .chat import chat_default
+from .profile import profile_default
+from .home import home_default
+
+__all__ = [
+    'login_default',
+    'settings_default',
+    'chat_default',
+    'user_profile_default'
+    'home_page_default'
+]
