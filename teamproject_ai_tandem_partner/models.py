3e5829ba6ea2bbc8e3a49e18f89395439faeb340
zunker323 <zunker323@outlook.de>
2024-11-22 12:31:34 +0100

    correct D100 value for ruff

---
+++ teamproject_ai_tandem_partner/models.py
@@ -0,0 +1,11 @@
+"""Module defining User database model."""
+
+import refelx as rx
+
+class User(rx.Model):  
+    """db model for users"""
+    
+    user_id: int
+    email: str
+    passwort: str
+    role: str
