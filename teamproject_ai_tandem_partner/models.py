8875b1bb45c7ac47fa61cd40356afef4be7802c7
Marcel <marcel.kraus@student.uni-tuebingen.de>
2024-12-03 16:35:19 +0100

    add User class and adjust DB url

--- teamproject_ai_tandem_partner/models.py
+++ teamproject_ai_tandem_partner/models.py
@@ -1,12 +1,45 @@
 """Module defining User database model."""
 
 import reflex as rx
+from passlib.context import CryptContext
+from sqlmodel import Field
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
-class User(rx.Model):
-    """db model for users"""
+class User(
+    rx.Model,
+    table=True,  # type: ignore
+):
+    """User model with bcrypt password hashing."""
 
-    user_id: int
-    email: str
-    passwort: str
-    role: str
+    email: str = Field(unique=True, nullable=False, index=True)
+    password_hash: str = Field(nullable=False)
+    enabled: bool = False
+    role: str = "student"
+
+    @staticmethod
+    def hash_password(secret: str) -> str:
+        """Hash the secret using bcrypt.
+
+        Args:
+            secret: The password to hash.
+
+        Returns:
+            The hashed password.
+        """
+        return pwd_context.hash(secret)
+
+    def verify(self, secret: str) -> bool:
+        """Validate the user's password.
+
+        Args:
+            secret: The password to check.
+
+        Returns:
+            True if the hashed secret matches this user's password_hash.
+        """
+        return pwd_context.verify(
+            secret,
+            self.password_hash,
+        )
