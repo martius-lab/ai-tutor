6c851f505c168dc33420245fdebf1f70e315b526
Marcel <marcel.kraus@student.uni-tuebingen.de>
2024-12-05 00:59:08 +0100

    add new graphical elements such as table header, search bar and refine design

--- aitandem/pages/add_excercises.py
+++ aitandem/pages/add_excercises.py
@@ -45,6 +45,7 @@ def add_exercise_button() -> rx.Component:
                 weight="medium",
                 text_align="left",
                 width="100%",
+                padding_bottom="0.5em",
             ),
             rx.input(
                 placeholder="exercise title",
@@ -57,12 +58,28 @@ def add_exercise_button() -> rx.Component:
                 weight="medium",
                 text_align="left",
                 width="100%",
+                padding_top="1.5em",
+                padding_bottom="0.5em",
             ),
             rx.text_area(
                 placeholder="describe the task here",
                 size="3",
                 width="100%",
                 height="150px",
+            ),
+            rx.text(
+                "Tag: ",
+                size="3",
+                weight="medium",
+                text_align="left",
+                width="100%",
+                padding_top="1.5em",
+                padding_bottom="0.5em",
+            ),
+            rx.input(
+                placeholder="set the tag for this task",
+                size="3",
+                width="100%",
             ),
             rx.flex(
                 rx.dialog.close(
@@ -73,8 +90,9 @@ def add_exercise_button() -> rx.Component:
                 ),
                 rx.form.submit(
                     rx.dialog.close(
-                        rx.button("Add Task"),
-                        style={"background-color": "green", "color": "white"},
+                        rx.button("Add Task",
+                                  color_scheme="grass",
+                                  ),
                     ),
                 ),
                 padding_top="1.5em",
@@ -82,13 +100,61 @@ def add_exercise_button() -> rx.Component:
                 justify="end",
             ),
         ),
+        unmount_on_exit=False,
     )
+
+
+def header_cell(text: str, icon: str):
+    return rx.table.column_header_cell(
+        rx.hstack(
+            rx.icon(icon, size=18),
+            rx.text(text),
+            align="center",
+            spacing="3",
+        ),
+    )
+
+
+def head_element():
+    return rx.fragment(
+        rx.flex(
+            rx.box(
+                rx.input(
+                    rx.input.slot(rx.icon("search")),
+                    placeholder="Search tasks...",
+                    size="3",
+                    max_width="250px",
+                ),
+                flex="1",
+            ),
+            rx.box(
+                add_exercise_button(),
+            ),
+            width="100%",
+        ),
+        rx.table.root(
+            rx.table.header(
+                rx.table.row(
+                    header_cell("Task", "briefcase-business"),
+                    header_cell("Description", "book-open-text"),
+                    header_cell("Tag", "tag"),
+                ),
+            ),
+            variant="surface",
+            size="3",
+            width="85vw",
+        ),
+    )
+
 
 
 @rx.page(route="/add-exercises")
 def add_exercises_default() -> rx.Component:
     """add exercises page."""
     return rx.center(
-        add_exercise_button(),
-        height="100vh",
+        rx.color_mode.button(position="top-right", type="button"),
+        rx.vstack(
+            head_element(),
+        ),
+        height="40vh",
     )
