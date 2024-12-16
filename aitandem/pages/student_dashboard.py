abf75e264849934b23e21b3a106c841bdf28e53f
jasmina <radici.jasmina@gmail.com>
2024-12-16 18:16:31 +0100

    added simple layout file for the dashboards

---
+++ aitandem/pages/student_dashboard.py
@@ -0,0 +1,4 @@
+import reflex as rx
+
+def student_dashboard_default() -> rx.Component:
+    return rx.box("Student dashboard")
---
+++ aitandem/pages/teacher_dashboard.py
@@ -0,0 +1,4 @@
+import reflex as rx
+
+def teacher_dashboard_default() -> rx.Component:
+    return rx.box("Teacher dashboard")
