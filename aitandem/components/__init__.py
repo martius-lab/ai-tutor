d975a20bd5546e20951f31f9041e691f732ada15
jasmina <radici.jasmina@gmail.com>
2024-12-16 18:06:56 +0100

    added user_roles function to init file

--- aitandem/components/__init__.py
+++ aitandem/components/__init__.py
@@ -3,6 +3,7 @@ from .chat_window import chat_window_default
 from .login_window import login_window_default
 from .settings_window import settings_window_default
 from .sidebar import sidebar_default
+from .user_roles import get_user_role
 
 __all__ = [
     "base_layout",
@@ -10,4 +11,5 @@ __all__ = [
     "login_window_default",
     "settings_window_default",
     "sidebar_default",
+    "get_user_role",
 ]
