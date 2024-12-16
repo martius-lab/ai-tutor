41298ceef5fd6403116c44bf1f9d5c26bac52221
jasmina <radici.jasmina@gmail.com>
2024-12-16 18:04:55 +0100

    added comments in user_roles file

--- aitandem/components/user_roles.py
+++ aitandem/components/user_roles.py
@@ -1,12 +1,19 @@
-"""Gets the users role"""
+"""retrieve the users role"""
 
 from aitandem.auth_session import AuthSession
 from sqlmodel import Session
 
 def get_user_role(session_id: str) -> str:
+    """
+    retrieve the users role based on the session id
+    Args:
+        session_id (str): The users session id
+    Returns:
+        str: The users role ("student" or "teacher"), or an error
+    """
     with Session() as session:
-        auth_session = session.get(AuthSession, session_id)
+        auth_session = session.get(AuthSession, session_id) # get the current session details
         if auth_session:
             return auth_session.role
         else:
-            pass
+            pass # include a corresponding error popup
