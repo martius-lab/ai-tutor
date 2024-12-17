54f66b96aa160d3ed7ea1cae07cff81d3270abce
jasmina <radici.jasmina@gmail.com>
2024-12-17 15:44:08 +0100

    added general state of popup dialog

--- aitandem/states/dialog_state.py
+++ aitandem/states/dialog_state.py
@@ -1,16 +1,16 @@
+"""Docstring."""
+
 import reflex as rx
-from aitandem.states.dialog_state import DialogState
 
 
-def error_popup(trigger_component, description: str):
-    return rx.dialog.root(
-        rx.dialog.trigger(trigger_component),
-        rx.dialog.content(
-            rx.dialog.title("Error!"),
-            rx.dialog.description(description),
-            rx.dialog.close(
-                rx.button("Close", size="3"),
-            ),
-        ),
-        on_open_change=DialogState.count_opens,
-    )
+class DialogState(rx.State):
+    """Docstring."""
+
+    num_opens: int = 0
+    opened: bool = False
+
+    @rx.event
+    def count_opens(self, value: bool):
+        """Docstring."""
+        self.opened = value
+        self.num_opens += 1
