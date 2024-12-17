54db02743ee0a091396b67e63d953aa4be009bdf
jasmina <radici.jasmina@gmail.com>
2024-12-17 15:50:45 +0100

    added clearer comments

--- aitandem/states/dialog_state.py
+++ aitandem/states/dialog_state.py
@@ -1,16 +1,22 @@
-"""Docstring."""
+"""This file manages the state for the popup dialog component
+It provides functionality to track whether the dialog is open or not
+"""
 
 import reflex as rx
 
 
 class DialogState(rx.State):
-    """Docstring."""
-
-    num_opens: int = 0
+    """Represents the state of a dialog component
+    Attributes:
+        opened (bool): Indicates whether the dialog is currently open
+    """
     opened: bool = False
 
     @rx.event
-    def count_opens(self, value: bool):
-        """Docstring."""
+    def set_opened(self, value: bool):
+        """Updates the dialog state
+        Args:
+            value (bool): A flag indicating whether the dialog is open (True) or closed (False).
+        This function sets the `opened` attribute to the given value.
+        """
         self.opened = value
-        self.num_opens += 1
