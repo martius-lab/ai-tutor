e7d999a0e9eac9145755af8202c8142ae74c6633
Felix Kloss <felix.kloss@uni-tuebingen.de>
2025-03-31 16:25:02 +0200

    Remove empty placeholder pages
    We can easily add them back once they are actually needed but for now
    remove them to get better overview over the existing code.

--- aitutor/pages/__init__.py
+++ aitutor/pages/__init__.py
@@ -1,7 +1,5 @@
 from .login import login_default
-from .settings import settings_default
 from .chat import chat_default
-from .profile import profile_default
 from .home import home_default
 from .registration import registration_default
 from .add_excercises import add_exercises_default
@@ -10,9 +8,7 @@ from .exercises import exercises_default
 
 __all__ = [
     "login_default",
-    "settings_default",
     "chat_default",
-    "profile_default",
     "home_default",
     "registration_default",
     "add_exercises_default",
