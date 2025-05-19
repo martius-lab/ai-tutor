71b706136c42d66b134c05833645fc9da531a989
Samuel Hempelmann <samuel.hempelmann@gmail.com>
2025-05-19 11:45:17 +0200

    change requests

--- aitutor/pages/__init__.py
+++ aitutor/pages/__init__.py
@@ -3,6 +3,7 @@ from aitutor.pages.home import home_default
 from aitutor.pages.add_excercises import add_exercises_default
 from aitutor.pages.exercises import exercises_default
 from aitutor.pages.navbar import navbar_default
+from aitutor.pages.not_found import not_found
 
 __all__ = [
     "chat_default",
@@ -10,4 +11,5 @@ __all__ = [
     "add_exercises_default",
     "exercises_default",
     "navbar_default",
+    "not_found",
 ]
