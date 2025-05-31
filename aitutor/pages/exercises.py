95d325294b3a12e568708d5aa9eca18cf40ace37
Samuel Hempelmann <samuel.hempelmann@gmail.com>
2025-05-31 13:19:03 +0200

    add submit status in exercises.py

--- aitutor/pages/exercises.py
+++ aitutor/pages/exercises.py
@@ -3,20 +3,27 @@ to view and add new exercises."""
 
 import reflex as rx
 from sqlmodel import select
-from typing import List
+from typing import List, Tuple
 
 from aitutor.models import Exercise
 from aitutor.pages.navbar import with_navbar
 from aitutor.auth.protection import require_role_at_least
-from aitutor.models import UserRole
+from aitutor.models import UserRole, ExerciseResult
+from aitutor.auth.state import SessionState
+
+from typing import Optional
+
+ExerciseWithResult = Tuple[Exercise, Optional[ExerciseResult]]
 
 
-class ExercisesState(rx.State):
+class ExercisesState(SessionState):
     """State for managing exercises."""
 
     exercises: List[Exercise] = []
     has_exercises: bool = False
     has_tags: bool = False
+    exercise_results: List[ExerciseResult] = []
+    exercises_with_result: List[ExerciseWithResult] = []
 
     @rx.event
     def fetch_exercises(self):
@@ -27,50 +34,101 @@ class ExercisesState(rx.State):
             exercises = session.exec(
                 select(Exercise)
             ).all()  # select all exercises from database
+            exercise_results = session.exec(
+                ExerciseResult.select().where(
+                    ExerciseResult.userinfo_id == self.user_id,
+                )
+            ).all()
+            self.exercise_results = list(exercise_results)
             self.exercises = list(exercises)
             self.has_exercises = len(exercises) > 0  # check if exercises exist
             self.has_tags = any(
                 len(exercise.tags) > 0 for exercise in exercises
             )  # check if tags exist
-
-
-def render_exercise_card(exercise: Exercise) -> rx.Component:
-    """Render exercises as cards"""
-    return rx.card(  # create a card for each exercise
-        rx.vstack(
-            rx.heading(exercise.title, size="6"),  # display title
-            rx.cond(  # display description if it exists
-                exercise.description is not None,
-                rx.hstack(
-                    rx.text("Description:", weight="bold", size="2"),
-                    rx.text(exercise.description, color="gray", size="2"),
-                    align_items="center",
-                    align="center",
-                ),
-            ),
-            rx.cond(  # display tags if they exist
-                ExercisesState.has_tags,
-                rx.hstack(
-                    rx.text("Tags:", weight="bold", size="2"),
-                    rx.hstack(
-                        rx.foreach(
-                            exercise.tags,
-                            lambda tag: rx.badge(
-                                tag, variant="soft", color_scheme="blue"
-                            ),
+            self.exercises_with_result = [
+                (
+                    exercise,
+                    next(
+                        (
+                            res
+                            for res in self.exercise_results
+                            if res.exercise_id == exercise.id
                         ),
-                        spacing="2",
+                        None,
+                    ),
+                )
+                for exercise in self.exercises
+            ]
+
+
+def show_exercise_status(exercise_id: int | None, submitted: bool) -> rx.Component:
+    """Display the submission status of an exercise."""
+    return rx.cond(
+        submitted,  # if the exercise has been submitted
+        rx.text(
+            "Submitted",
+            color="green",
+        ),
+        rx.text(
+            "Not submitted",
+            color="red",
+        ),
+    )
+
+
+def render_exercise_card(exercises_with_res: ExerciseWithResult) -> rx.Component:
+    """Render exercises as cards"""
+    exercise: Exercise = exercises_with_res[0]
+    result: ExerciseResult | None = exercises_with_res[1]
+    is_submitted = (
+        False
+        if result is None
+        else (
+            result.finished_conversation is not None
+            and result.finished_conversation.length() > 0  # type: ignore
+        )
+    )  # type: ignore
+    return rx.card(  # create a card for each exercise
+        rx.hstack(
+            rx.vstack(
+                rx.heading(exercise.title, size="6"),  # display title
+                rx.cond(  # display description if it exists
+                    exercise.description is not None,
+                    rx.hstack(
+                        rx.text("Description:", weight="bold", size="2"),
+                        rx.text(exercise.description, color="gray", size="2"),
+                        align_items="center",
+                        align="center",
                     ),
                 ),
-            ),
-            rx.cond(  # display image if it exists
-                exercise.image is not None,
-                rx.image(
-                    src=exercise.image, width="200px", height="auto", border_radius="md"
+                rx.cond(  # display tags if they exist
+                    ExercisesState.has_tags,
+                    rx.hstack(
+                        rx.text("Tags:", weight="bold", size="2"),
+                        rx.hstack(
+                            rx.foreach(
+                                exercise.tags,
+                                lambda tag: rx.badge(
+                                    tag, variant="soft", color_scheme="blue"
+                                ),
+                            ),
+                            spacing="2",
+                        ),
+                    ),
                 ),
+                rx.cond(  # display image if it exists
+                    exercise.image is not None,
+                    rx.image(
+                        src=exercise.image,
+                        width="200px",
+                        height="auto",
+                        border_radius="md",
+                    ),
+                ),
+                spacing="2",
+                align="start",
             ),
-            spacing="2",
-            align="start",
+            show_exercise_status(exercise.id, is_submitted),
         ),
         variant="surface",
         width="100%",
@@ -87,7 +145,7 @@ def render_exercises() -> rx.Component:
         ExercisesState.has_exercises,  # if they exist
         rx.vstack(
             rx.foreach(
-                ExercisesState.exercises,  # iterate through exercises and render cards
+                ExercisesState.exercises_with_result,
                 render_exercise_card,
             ),
             spacing="4",
