e08f048bb99d76aa5aa0afb895176c3cdb995ae6
Samuel Hempelmann <samuel.hempelmann@gmail.com>
2025-06-29 15:57:23 +0200

    add navigation to finished view

--- aitutor/pages/submissions/state.py
+++ aitutor/pages/submissions/state.py
@@ -5,6 +5,7 @@ import reflex as rx
 from reflex_local_auth.user import LocalUser
 from aitutor.models import ExerciseResult, UserInfo, Exercise, UserRole
 from sqlmodel import and_, select
+from aitutor import routes
 
 
 class SubmissionsState(rx.State):
@@ -12,6 +13,14 @@ class SubmissionsState(rx.State):
 
     exercise_title: str = ""
     table_rows: list[tuple[str, str, bool]]  # name, role, has_submitted
+
+    @rx.var
+    def finished_view_url(self) -> str:
+        """
+        The exercise_id is used to identify the current exercise.
+        It is set by the route parameter in the URL.
+        """
+        return f"{routes.FINISHED_VIEW}/{self.router.page.params.get('exercise_id', 0)}"
 
     @rx.event
     def on_load(self):
