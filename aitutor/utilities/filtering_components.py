8e5e01178da5073a359d2ceaf7257ba83a8de8ef
Samuel Hempelmann <samuel.hempelmann@gmail.com>
2025-08-07 14:06:41 +0200

    move search_bar and search_badges to utilities file

---
+++ aitutor/utilities/filtering_components.py
@@ -0,0 +1,83 @@
+"""Components that are used in the frontend for filtering tables"""
+
+import reflex as rx
+from aitutor.global_vars import USER_KEY, EXERCISE_KEY, TAG_KEY
+
+
+def search_bar(state) -> rx.Component:
+    """Search bar for the input of search values."""
+    return rx.form.root(
+        rx.hstack(
+            rx.dialog.root(
+                rx.dialog.trigger(
+                    rx.icon("info"),
+                    _hover={"cursor": "pointer"},
+                ),
+                rx.dialog.content(
+                    rx.vstack(
+                        rx.text(
+                            "Search with 'key:searchValue' or "
+                            "'key:\"search value\"' "
+                            "to search a specific column."
+                        ),
+                        rx.text("keys: exercise, tag"),
+                        rx.text("Without using 'key:' it searches in all columns."),
+                    ),
+                ),
+            ),
+            rx.input(
+                rx.input.slot(rx.icon("search")),
+                name="search_value",
+                placeholder="tag:tagname",
+                required=True,
+            ),
+            rx.button(
+                rx.icon("plus"),
+                _hover={"cursor": "pointer"},
+            ),
+            justify="center",
+            align="center",
+        ),
+        on_submit=state.add_search_value,  # needs to be called the same in all states
+        reset_on_submit=True,
+        max_width="250px",
+    )
+
+
+def search_badges(state) -> rx.Component:
+    """Display search badges for the current search values."""
+    return rx.hstack(
+        rx.foreach(
+            state.search_values,  # needs to be called the same in all states
+            lambda value: rx.badge(
+                rx.hstack(
+                    rx.cond(
+                        value[0] == USER_KEY,
+                        rx.icon("user-round", size=18),
+                    ),
+                    rx.cond(
+                        value[0] == EXERCISE_KEY,
+                        rx.icon("book", size=18),
+                    ),
+                    rx.cond(
+                        value[0] == TAG_KEY,
+                        rx.icon("tag", size=18),
+                    ),
+                    rx.text(value[1]),
+                    rx.icon(
+                        "x",
+                        on_click=state.remove_search_value(
+                            value
+                        ),  # needs to be called the same in all states
+                        _hover={"cursor": "pointer"},
+                    ),
+                    spacing="1",
+                    align="center",
+                ),
+                variant="solid",
+                color_scheme="blue",
+            ),
+        ),
+        spacing="2",
+        wrap="wrap",
+    )
