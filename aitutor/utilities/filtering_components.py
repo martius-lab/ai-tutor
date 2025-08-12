984b4ae96f229b7032ec626063d7328e18b98d2d
Samuel Hempelmann <samuel.hempelmann@gmail.com>
2025-08-12 13:38:21 +0200

    use mixin to create a filtering class

--- aitutor/utilities/filtering_components.py
+++ aitutor/utilities/filtering_components.py
@@ -1,10 +1,45 @@
 """Components that are used in the frontend for filtering tables"""
 
 import reflex as rx
+from abc import abstractmethod
+import contextlib
+
 from aitutor.global_vars import SEARCH_USER_KEY, SEARCH_EXERCISE_KEY, SEARCH_TAG_KEY
+from aitutor.utilities.parser import parse_query_keys
 
 
-def search_bar(state, keys_info: str) -> rx.Component:
+class FilterMixin(rx.State, mixin=True):
+    """Mixin for filtering components."""
+
+    # a list of search values, each a tuple of (key, value)
+    search_values: list[tuple[str, str]] = []
+
+    # valid search keys. Needs to be overridden by parent class
+    search_keys: list[str] = []
+
+    @abstractmethod
+    @rx.event
+    def load_filtered_data(self):
+        """Implement in child state to load data after filter changes."""
+        raise NotImplementedError
+
+    @rx.event
+    def add_search_value(self, form_data: dict):
+        """Adds a search value to the list of search values."""
+        parsed = parse_query_keys(form_data["search_value"], self.search_keys)
+        if parsed not in self.search_values:
+            self.search_values.append(parsed)
+        self.load_filtered_data()
+
+    @rx.event
+    def remove_search_value(self, value: tuple[str, str]):
+        """Removes a search value from the list."""
+        with contextlib.suppress(ValueError):
+            self.search_values.remove(tuple[str, str](value))
+        self.load_filtered_data()
+
+
+def search_bar(state) -> rx.Component:
     """Search bar for the input of search values."""
     return rx.form.root(
         rx.hstack(
@@ -20,7 +55,7 @@ def search_bar(state, keys_info: str) -> rx.Component:
                             "'key:\"search value\"' "
                             "to search a specific column."
                         ),
-                        rx.text(f"keys: {keys_info}"),
+                        rx.text(f"keys: {state.search_keys}"),
                         rx.text("Without using 'key:' it searches in all columns."),
                     ),
                 ),
@@ -38,7 +73,7 @@ def search_bar(state, keys_info: str) -> rx.Component:
             justify="center",
             align="center",
         ),
-        on_submit=state.add_search_value,  # needs to be called the same in all states
+        on_submit=state.add_search_value,
         reset_on_submit=True,
     )
 
@@ -47,7 +82,7 @@ def search_badges(state) -> rx.Component:
     """Display search badges for the current search values."""
     return rx.hstack(
         rx.foreach(
-            state.search_values,  # needs to be called the same in all states
+            state.search_values,
             lambda value: rx.badge(
                 rx.hstack(
                     rx.cond(
@@ -65,9 +100,7 @@ def search_badges(state) -> rx.Component:
                     rx.text(value[1]),
                     rx.icon(
                         "x",
-                        on_click=state.remove_search_value(
-                            value
-                        ),  # needs to be called the same in all states
+                        on_click=state.remove_search_value(value),
                         _hover={"cursor": "pointer"},
                     ),
                     spacing="1",
