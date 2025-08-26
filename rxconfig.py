ba02406e41be17960f0e49ce1932709765e82f30
Felix Kloss <felix.kloss@uni-tuebingen.de>
2025-08-26 18:47:48 +0200

    Remove (apparently non-existing?) watch_ignore_patterns
    Linting complained about this parameter not existing.  I also couldn't
    find anything via Google or on the Reflex website.  Maybe it was
    erroneously added at some point?
    I simply remove it now.  Let's keep an eye on it if we notice any
    undesired rebuilds.

--- rxconfig.py
+++ rxconfig.py
@@ -6,15 +6,6 @@ config = rx.Config(
     app_name="aitutor",
     db_url="sqlite:///reflex.db",
     plugins=[rx.plugins.SitemapPlugin()],
-    watch_ignore_patterns=[
-        ".venv/*",
-        ".git/*",
-        "alembic/*",
-        "build/*",
-        "dist/*",
-        "*.pyc",
-        "__pycache__/*",
-    ],
     telemetry_enabled=False,
     show_built_with_reflex=False,
 )
