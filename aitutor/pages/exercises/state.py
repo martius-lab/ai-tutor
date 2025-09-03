9e5416ce77cb5b9291a5befdfe7c1b310afaf849
Samuel Hempelmann <samuel.hempelmann@gmail.com>
2025-09-03 11:55:37 +0200

    remove unneeded state vars has_tags and has_exercises + remove unnecessary helper functions

--- aitutor/pages/exercises/state.py
+++ aitutor/pages/exercises/state.py
@@ -4,7 +4,7 @@ import reflex as rx
 from sqlmodel import and_, select
 from sqlalchemy.orm import selectinload
 from typing import Optional
-from datetime import datetime, timedelta
+from datetime import datetime
 from zoneinfo import ZoneInfo
 
 from aitutor.models import Exercise, ExerciseResult, UserRole
@@ -19,8 +19,6 @@ ExerciseWithResult = tuple[Exercise, Optional[ExerciseResult]]
 class ExercisesState(SessionState):
     """State for managing exercises."""
 
-    has_exercises: bool = False
-    has_tags: bool = False
     exercises_with_result: list[ExerciseWithResult] = []
     deadline_strings: dict[int, str] = {}  # (exercise_id, deadline_string)
     time_left_strings: dict[int, str] = {}  # (exercise_id, time_left_string)
@@ -73,49 +71,22 @@ class ExercisesState(SessionState):
             ).all()
             self.exercises_with_result = [(x[0], x[1]) for x in exercises_with_result]
 
-            self.exercises_with_result = self.hide_not_started_exercises()
+            # set not started exercises as hidden
+            for exercise, _ in self.exercises_with_result:
+                if not exercise.is_started:
+                    exercise.is_hidden = True
 
             # if the user is a student, remove not started exercises
             assert self.user_role is not None, "User role not set.  This is a bug."
             if self.user_role < UserRole.TEACHER:
-                self.exercises_with_result = self.remove_not_started_exercises()
+                exercises_with_result = [
+                    ex_res
+                    for ex_res in self.exercises_with_result
+                    if not ex_res[0].is_hidden
+                ]
 
-            self.has_exercises = len(self.exercises_with_result) > 0
-            self.has_tags = any(
-                len(exercise.tags) > 0 for exercise, _ in self.exercises_with_result
-            )
-            self.generate_deadline_strings()
             self.update_time_left_strings()
-
-    def hide_not_started_exercises(self) -> list[ExerciseWithResult]:
-        """Hide exercises that have not started yet."""
-        # set is_hidden for not started exercises
-        exercises_with_result = self.exercises_with_result
-        for exercise, _ in exercises_with_result:
-            if exercise.deadline and exercise.days_to_complete:
-                end = exercise.deadline.replace(tzinfo=ZoneInfo(TIME_ZONE))
-                start = end - timedelta(days=exercise.days_to_complete)
-                current_time = datetime.now(ZoneInfo(TIME_ZONE))
-                if current_time < start:
-                    exercise.is_hidden = True
-        return exercises_with_result
-
-    def remove_not_started_exercises(self) -> list[ExerciseWithResult]:
-        """remove non started exercises"""
-        exercises_with_result = [
-            ex_res for ex_res in self.exercises_with_result if not ex_res[0].is_hidden
-        ]
-        return exercises_with_result
-
-    def generate_deadline_strings(self):
-        """Get the deadline string for every exercise."""
-        for exercise, _ in self.exercises_with_result:
-            if exercise.deadline:
-                self.deadline_strings[exercise.id] = exercise.deadline.strftime(  # type: ignore
-                    "%d.%m.%Y, %H:%MUhr"
-                )
-            else:
-                self.deadline_strings[exercise.id] = "No deadline"  # type: ignore
+            self.generate_deadline_strings()
 
     @rx.event
     def update_time_left_strings(self):
@@ -131,10 +102,18 @@ class ExercisesState(SessionState):
                 if time_left.total_seconds() <= 0:
                     self.time_left_strings[exercise.id] = "deadline has passed"  # type: ignore
 
+    def generate_deadline_strings(self):
+        """Get the deadline string for every exercise."""
+        for exercise, _ in self.exercises_with_result:
+            if exercise.deadline:
+                self.deadline_strings[exercise.id] = exercise.deadline.strftime(  # type: ignore
+                    "%d.%m.%Y, %H:%MUhr"
+                )
+            else:
+                self.deadline_strings[exercise.id] = "No deadline"  # type: ignore
+
     def on_logout(self):
         """Clears the state when the user logs out."""
-        self.has_exercises = False
-        self.has_tags = False
         self.exercises_with_result = []
         self.deadline_strings = {}
         self.time_left_strings = {}
