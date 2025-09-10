ae628065df08cacc718904606be343c6b7ee0299
Samuel Hempelmann <samuel.hempelmann@gmail.com>
2025-09-10 15:45:57 +0200

    move 'Add Exercise' button back to the top right of the table

--- aitutor/pages/manage_exercises/components.py
+++ aitutor/pages/manage_exercises/components.py
@@ -208,7 +208,7 @@ def exercise_table():
                 size="3",
                 width="85vw",
                 overflow_y="auto",
-                max_height="60vh",
+                max_height="66vh",
             ),
             edit_exercise_dialog(),
         ),
