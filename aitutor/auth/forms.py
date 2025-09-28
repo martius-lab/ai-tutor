3efc33f25ff231aa6c042959ff940c3cb92dadad
Samuel Hempelmann <samuel.hempelmann@gmail.com>
2025-09-28 13:05:00 +0200

    - clear error/success messages on load in login & registration - clear input fields after successful registration - add success message after successful registration - make all inputs in login & registration mandatory

--- aitutor/auth/forms.py
+++ aitutor/auth/forms.py
@@ -44,9 +44,18 @@ def my_login_form() -> rx.Component:
             rx.heading(LanguageState.login_heading, size="7"),
             login_error(),
             rx.text(LanguageState.username),
-            input("username", placeholder=LanguageState.username),
+            input(
+                "username",
+                placeholder=LanguageState.username,
+                required=True,
+            ),
             rx.text(LanguageState.password),
-            input("password", placeholder=LanguageState.password, type="password"),
+            input(
+                "password",
+                placeholder=LanguageState.password,
+                type="password",
+                required=True,
+            ),
             rx.button(LanguageState.log_in, width="100%", _hover={"cursor": "pointer"}),
             rx.center(
                 rx.link(LanguageState.register, on_click=rx.redirect(routes.REGISTER)),
@@ -61,11 +70,25 @@ def my_login_form() -> rx.Component:
 def register_error() -> rx.Component:
     """Render the registration error message."""
     return rx.cond(
-        reflex_local_auth.RegistrationState.error_message != "",
+        MyRegisterState.error_message != "",
         rx.callout(
-            reflex_local_auth.RegistrationState.error_message,
+            MyRegisterState.error_message,
             icon="triangle_alert",
             color_scheme="red",
+            role="alert",
+            width="100%",
+        ),
+    )
+
+
+def success_message() -> rx.Component:
+    """Render the successful registration message."""
+    return rx.cond(
+        MyRegisterState.success,
+        rx.callout(
+            LanguageState.successful_registration,
+            icon="check",
+            color_scheme="green",
             role="alert",
             width="100%",
         ),
@@ -78,19 +101,41 @@ def my_register_form() -> rx.Component:
         rx.vstack(
             rx.heading(LanguageState.register_heading, size="7"),
             register_error(),
+            success_message(),
             rx.text(LanguageState.username),
-            input("username", placeholder=LanguageState.username),
+            input(
+                "username",
+                placeholder=LanguageState.username,
+                required=True,
+                value=MyRegisterState.username,
+                on_change=MyRegisterState.set_username,
+            ),
             rx.text(LanguageState.email),
             input(
-                "email", placeholder=LanguageState.email, type="email", required=True
+                "email",
+                placeholder=LanguageState.email,
+                type="email",
+                required=True,
+                value=MyRegisterState.email,
+                on_change=MyRegisterState.set_email,
             ),
             rx.text(LanguageState.password),
-            input("password", placeholder=LanguageState.password, type="password"),
+            input(
+                "password",
+                placeholder=LanguageState.password,
+                type="password",
+                required=True,
+                value=MyRegisterState.password,
+                on_change=MyRegisterState.set_password,
+            ),
             rx.text(LanguageState.confirm_password),
             input(
                 "confirm_password",
                 placeholder=LanguageState.confirm_password,
                 type="password",
+                required=True,
+                value=MyRegisterState.confirm_password,
+                on_change=MyRegisterState.set_confirm_password,
             ),
             rx.button(
                 LanguageState.register,
@@ -106,5 +151,5 @@ def my_register_form() -> rx.Component:
             ),
             min_width=MIN_WIDTH,
         ),
-        on_submit=MyRegisterState.handle_registration_email,  # type: ignore
+        on_submit=MyRegisterState.handle_custom_registration,
     )
