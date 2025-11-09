650fb67aa47c7f017a2c590603a8fbbb30ad067a
Samuel Hempelmann <samuel.hempelmann@gmail.com>
2025-11-09 14:21:42 +0100

    add config table in the database and use it in the code

--- aitutor/aitutor.py
+++ aitutor/aitutor.py
@@ -5,12 +5,14 @@ This module contains the main app definition for Reflex.
 
 import sys
 import decouple
+import sqlmodel
 
 import reflex as rx
 
 from aitutor import pages
-from aitutor.config import get_config
+from aitutor.config import get_config, get_config_file
 from aitutor.utilities.create_default_users import create_default_users
+from aitutor.models import Config
 import aitutor.routes as routes
 
 # info: add dynamic routes first
@@ -81,19 +83,40 @@ app.add_page(pages.impressum_page, route=routes.IMPRESSUM)
 app.add_page(pages.privacy_notice_page, route=routes.PRIVACY_NOTICE)
 
 
-async def initialize():
+def initialize():
     """Initialization steps that are run once when the app starts."""
-    # Note that this function is run as an asynchronous lifespan task, so the
-    # application doesn't actually wait for it to be finished before starting to serve
-    # requests.  So we should be careful if we ever add any longer-running steps here.
+
+    print("Executing initialization tasks:")
+
+    # ensure there is a config row in the database
+    with rx.session() as session:
+        config_row = session.exec(
+            sqlmodel.select(Config).where(Config.id == 1)
+        ).one_or_none()
+        if not config_row:
+            config = Config(
+                id=1,
+                check_conversation_prompt=get_config_file().check_conversation_prompt,
+                response_ai_model=get_config_file().response_ai_model,
+                check_ai_model=get_config_file().check_ai_model,
+                how_to_use_text=get_config_file().how_to_use_text,
+                general_information_text=get_config_file().general_information_text,
+                lecture_information_text=get_config_file().lecture_information_text,
+                course_name=get_config_file().course_name,
+                impressum_text=get_config_file().impressum_text,
+                registration_code=get_config_file().registration_code,
+            )
+            session.add(config)
+            session.commit()
+            print("Configuration row added to the database.")
+        else:
+            print("Configuration row already exists in the database.")
 
     # load config here, so we fail immediately if there is any issue with it
     try:
         config = get_config()
-        print(
-            f"Using {config.response_ai_model} for responses "
-            + f"and {config.check_ai_model} for checks."
-        )
+        if config.course_name:
+            print("Configuration can be loaded successfully.")
     except Exception as e:
         print("\033[91m" + f"Error loading config: {e}" + "\033[0m")
         sys.exit(1)
@@ -107,8 +130,12 @@ async def initialize():
             + "\033[0m"
         )
         sys.exit(1)
+    else:
+        print("OPENAI_API_KEY found in environment variables.")
 
     create_default_users()
+
+    print("Initialization tasks completed.")
 
 
 app.register_lifespan_task(initialize)
--- aitutor/config.py
+++ aitutor/config.py
@@ -1,8 +1,11 @@
 """Configuration management of the application."""
 
+import variconf
+import reflex as rx
+import sqlmodel
 from dataclasses import dataclass
 
-import variconf
+from aitutor.models import Config
 
 
 DEFAULT_CONFIG_FILE_PATH = "./default_config.toml"
@@ -44,12 +47,12 @@ class AiTutorConfig:
     exercise_prompts: list[ConfigExercisePrompt]
 
 
-_config = None
+_config_from_file = None
 
 
-def load_config():
+def load_config_file():
     """Load configuration from file."""
-    global _config
+    global _config_from_file
     wconf = variconf.WConf(AiTutorConfig)
     try:
         wconf.load_file(CONFIG_FILE_PATH)
@@ -59,13 +62,37 @@ def load_config():
         )
         wconf.load_file(DEFAULT_CONFIG_FILE_PATH)
 
-    _config = wconf.get()
+    _config_from_file = wconf.get()
+
+
+def get_config_file() -> AiTutorConfig:
+    """Get the content of the configuration file."""
+    if _config_from_file is None:
+        load_config_file()
+    # Type of _config_from_file is actually some OmegaConf object, but it should have
+    # the same fields as AiTutorConfig, so list that as return type for better
+    # auto-completion.
+    return _config_from_file  # type: ignore[return-value]
 
 
 def get_config() -> AiTutorConfig:
-    """Get the configuration object."""
-    if _config is None:
-        load_config()
-    # Type of _config is actually some OmegaConf object, but it should have the same
-    # fields as AiTutorConfig, so list that as return type for better auto-completion.
-    return _config  # type: ignore[return-value]
+    """Get the configuration from the database."""
+    with rx.session() as session:
+        _config = session.exec(
+            sqlmodel.select(Config).where(Config.id == 1)
+        ).one_or_none()
+        if _config is None:
+            raise ValueError("Configuration not found in the database.")
+        return AiTutorConfig(
+            check_conversation_prompt=_config.check_conversation_prompt,
+            response_ai_model=_config.response_ai_model,
+            check_ai_model=_config.check_ai_model,
+            how_to_use_text=_config.how_to_use_text,
+            general_information_text=_config.general_information_text,
+            lecture_information_text=_config.lecture_information_text,
+            course_name=_config.course_name,
+            impressum_text=_config.impressum_text,
+            registration_code=_config.registration_code,
+            default_users=get_config_file().default_users,
+            exercise_prompts=get_config_file().exercise_prompts,
+        )
