dcbb1221195c8fd05ed47c1e926bfb9c2c612851
Samuel Hempelmann <samuel.hempelmann@gmail.com>
2025-11-11 18:18:15 +0100

    move configuration settings out of the dialog and add indicators for unsaved changes

--- aitutor/pages/configuration/components.py
+++ aitutor/pages/configuration/components.py
@@ -86,122 +86,126 @@ def info_icon(info_text: str | rx.Var[str]) -> rx.Component:
 
 def config_form() -> rx.Component:
     """Returns input fields for configuration settings."""
-    return rx.dialog.root(
-        rx.dialog.trigger(
-            rx.button(
-                "Config settings",
-                _hover={"cursor": "pointer"},
-                on_click=ConfigurationState.set_config_dialog_open(True),
-            ),
-        ),
-        rx.dialog.content(
-            rx.form(
-                rx.vstack(
-                    input(
-                        name="course_name",
-                        heading=LS.course_name,
-                        value=ConfigurationState.current_config.course_name,
-                        on_change=lambda value: ConfigurationState.set_config_value(
-                            "course_name", value
-                        ),
+    return rx.card(
+        rx.form(
+            rx.vstack(
+                input(
+                    name="course_name",
+                    heading=LS.course_name,
+                    value=ConfigurationState.current_config.course_name,
+                    on_change=lambda value: ConfigurationState.set_config_value(
+                        "course_name", value
                     ),
-                    input(
-                        name="registration_code",
-                        heading=LS.registration_code,
-                        value=ConfigurationState.current_config.registration_code,
-                        on_change=lambda value: ConfigurationState.set_config_value(
-                            "registration_code", value
-                        ),
-                        info=info_icon(LS.registration_code_info),
-                    ),
-                    input(
-                        name="response_ai_model",
-                        heading=LS.response_ai_model,
-                        value=ConfigurationState.current_config.response_ai_model,
-                        on_change=lambda value: ConfigurationState.set_config_value(
-                            "response_ai_model", value
-                        ),
-                        info=info_icon(LS.response_ai_model_info),
-                    ),
-                    input(
-                        name="check_ai_model",
-                        heading=LS.check_ai_model,
-                        value=ConfigurationState.current_config.check_ai_model,
-                        on_change=lambda value: ConfigurationState.set_config_value(
-                            "check_ai_model", value
-                        ),
-                        info=info_icon(LS.check_ai_model_info),
-                    ),
-                    text_area(
-                        name="check_conversation_prompt",
-                        heading=LS.check_conversation_prompt,
-                        value=ConfigurationState.current_config.check_conversation_prompt,
-                        on_change=lambda value: ConfigurationState.set_config_value(
-                            "check_conversation_prompt", value
-                        ),
-                        info=info_icon(LS.check_conversation_prompt_info),
-                    ),
-                    text_area(
-                        name="how_to_use_text",
-                        heading=LS.how_to_use_text,
-                        value=ConfigurationState.current_config.how_to_use_text,
-                        on_change=lambda value: ConfigurationState.set_config_value(
-                            "how_to_use_text", value
-                        ),
-                        info=info_icon(LS.info_texts_info),
-                    ),
-                    text_area(
-                        name="general_info_text",
-                        heading=LS.general_info_text,
-                        value=ConfigurationState.current_config.general_information_text,
-                        on_change=lambda value: ConfigurationState.set_config_value(
-                            "general_information_text", value
-                        ),
-                        info=info_icon(LS.info_texts_info),
-                    ),
-                    text_area(
-                        name="lecture_info_text",
-                        heading=LS.lecture_info_text,
-                        value=ConfigurationState.current_config.lecture_information_text,
-                        on_change=lambda value: ConfigurationState.set_config_value(
-                            "lecture_information_text", value
-                        ),
-                        info=info_icon(LS.info_texts_info),
-                    ),
-                    text_area(
-                        name="impressum",
-                        heading=LS.impressum,
-                        value=ConfigurationState.current_config.impressum_text,
-                        on_change=lambda value: ConfigurationState.set_config_value(
-                            "impressum_text", value
-                        ),
-                        info=info_icon(LS.impressum_info),
-                    ),
-                    rx.hstack(
-                        rx.button(
-                            LS.cancel,
-                            color_scheme="red",
-                            _hover={"cursor": "pointer"},
-                            type="button",
-                            on_click=ConfigurationState.set_config_dialog_open(False),
-                        ),
-                        rx.button(
-                            LS.save,
-                            _hover={"cursor": "pointer"},
-                            type="submit",
-                        ),
-                        justify="end",
-                        width="100%",
-                    ),
-                    spacing="3",
                 ),
-                on_submit=[
-                    ConfigurationState.save_config_to_db(),
-                    ConfigurationState.set_config_dialog_open(False),
-                ],
+                input(
+                    name="registration_code",
+                    heading=LS.registration_code,
+                    value=ConfigurationState.current_config.registration_code,
+                    on_change=lambda value: ConfigurationState.set_config_value(
+                        "registration_code", value
+                    ),
+                    info=info_icon(LS.registration_code_info),
+                ),
+                input(
+                    name="response_ai_model",
+                    heading=LS.response_ai_model,
+                    value=ConfigurationState.current_config.response_ai_model,
+                    on_change=lambda value: ConfigurationState.set_config_value(
+                        "response_ai_model", value
+                    ),
+                    info=info_icon(LS.response_ai_model_info),
+                ),
+                input(
+                    name="check_ai_model",
+                    heading=LS.check_ai_model,
+                    value=ConfigurationState.current_config.check_ai_model,
+                    on_change=lambda value: ConfigurationState.set_config_value(
+                        "check_ai_model", value
+                    ),
+                    info=info_icon(LS.check_ai_model_info),
+                ),
+                text_area(
+                    name="check_conversation_prompt",
+                    heading=LS.check_conversation_prompt,
+                    value=ConfigurationState.current_config.check_conversation_prompt,
+                    on_change=lambda value: ConfigurationState.set_config_value(
+                        "check_conversation_prompt", value
+                    ),
+                    info=info_icon(LS.check_conversation_prompt_info),
+                ),
+                text_area(
+                    name="how_to_use_text",
+                    heading=LS.how_to_use_text,
+                    value=ConfigurationState.current_config.how_to_use_text,
+                    on_change=lambda value: ConfigurationState.set_config_value(
+                        "how_to_use_text", value
+                    ),
+                    info=info_icon(LS.info_texts_info),
+                ),
+                text_area(
+                    name="general_info_text",
+                    heading=LS.general_info_text,
+                    value=ConfigurationState.current_config.general_information_text,
+                    on_change=lambda value: ConfigurationState.set_config_value(
+                        "general_information_text", value
+                    ),
+                    info=info_icon(LS.info_texts_info),
+                ),
+                text_area(
+                    name="lecture_info_text",
+                    heading=LS.lecture_info_text,
+                    value=ConfigurationState.current_config.lecture_information_text,
+                    on_change=lambda value: ConfigurationState.set_config_value(
+                        "lecture_information_text", value
+                    ),
+                    info=info_icon(LS.info_texts_info),
+                ),
+                text_area(
+                    name="impressum",
+                    heading=LS.impressum,
+                    value=ConfigurationState.current_config.impressum_text,
+                    on_change=lambda value: ConfigurationState.set_config_value(
+                        "impressum_text", value
+                    ),
+                    info=info_icon(LS.impressum_info),
+                ),
+                rx.cond(
+                    ConfigurationState.unsaved_changes,
+                    rx.callout(
+                        LS.unsaved_changes_info,
+                        icon="info",
+                        width="100%",
+                        color_scheme="orange",
+                    ),
+                ),
+                rx.hstack(
+                    rx.button(
+                        LS.cancel,
+                        color_scheme="red",
+                        _hover={"cursor": "pointer"},
+                        type="button",
+                        on_click=ConfigurationState.on_load(),
+                        disabled=~ConfigurationState.unsaved_changes,  # type: ignore
+                    ),
+                    rx.button(
+                        LS.save,
+                        _hover={"cursor": "pointer"},
+                        type="submit",
+                        disabled=~ConfigurationState.unsaved_changes,  # type: ignore
+                    ),
+                    justify="end",
+                    width="100%",
+                ),
+                spacing="3",
             ),
+            on_submit=ConfigurationState.save_config_to_db(),
             width="40em",
             max_width="90vw",
         ),
-        open=ConfigurationState.config_dialog_open,
+        outline=rx.cond(
+            ConfigurationState.unsaved_changes,
+            "1px solid orange",
+            "none",
+        ),
+        variant="ghost",
     )
