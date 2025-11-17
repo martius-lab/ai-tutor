1cb3a2e50157e4227cd72ee4b7de75ae69b62288
Andrewtuebingen <andrew.aleid@student.uni-tuebingen.de>
2025-11-17 12:21:14 +0100

    refactoring 3 buttons

--- aitutor/pages/chat/components.py
+++ aitutor/pages/chat/components.py
@@ -4,6 +4,7 @@ import reflex as rx
 
 from aitutor.pages.chat.state import ChatState, ChatMessage, Role
 from aitutor.language_state import LanguageState
+from aitutor.components import destructive_confirm
 
 
 def message_box(chat_message: ChatMessage) -> rx.Component:
@@ -135,58 +136,41 @@ def chat_form() -> rx.Component:
     )
 
 
+
 def edit_last_message_button() -> rx.Component:
     """
     Render the button to delete the last message.
     """
-    return rx.alert_dialog.root(
-        rx.alert_dialog.trigger(
-            rx.button(
-                rx.hstack(
-                    rx.icon("trash", size=20),
-                    rx.text("+", size="4"),
-                    rx.icon("pencil", size=20),
-                    spacing="1",
-                    align="center",
-                    justify="center",
-                ),
-                color_scheme="iris",
-                _hover=rx.cond(
-                    ChatState.waiting_for_response,
-                    {"cursor": "not-allowed"},
-                    {"cursor": "pointer"},
-                ),
-                disabled=rx.cond(
-                    ChatState.waiting_for_response,
-                    True,
-                    False,
-                ),
-                type="button",
-            ),
-        ),
-        rx.alert_dialog.content(
-            rx.alert_dialog.title(LanguageState.edit_last_message),
-            rx.alert_dialog.description(LanguageState.edit_last_message_info),
+    return destructive_confirm(
+        title=LanguageState.edit_last_message,
+        description=LanguageState.edit_last_message_info,
+        confirm_text=LanguageState.confirm,
+        cancel_text=LanguageState.cancel,
+        on_confirm=ChatState.edit_last_message,
+        trigger=rx.button(
             rx.hstack(
-                rx.alert_dialog.cancel(
-                    rx.button(
-                        LanguageState.cancel,
-                        color_scheme="red",
-                        _hover={"cursor": "pointer"},
-                    ),
-                ),
-                rx.alert_dialog.action(
-                    rx.button(
-                        LanguageState.confirm,
-                        color_scheme="iris",
-                        on_click=ChatState.edit_last_message,
-                        _hover={"cursor": "pointer"},
-                    ),
-                ),
-                margin_top="1em",
+                rx.icon("trash", size=20),
+                rx.text("+", size="4"),
+                rx.icon("pencil", size=20),
+                spacing="1",
+                align="center",
+                justify="center",
             ),
+            color_scheme="iris",
+            _hover=rx.cond(
+                ChatState.waiting_for_response,
+                {"cursor": "not-allowed"},
+                {"cursor": "pointer"},
+            ),
+            disabled=rx.cond(
+                ChatState.waiting_for_response,
+                True,
+                False,
+            ),
+            type="button",
         ),
     )
+
 
 
 def send_message_button() -> rx.Component:
@@ -207,57 +191,31 @@ def send_message_button() -> rx.Component:
 
 
 def reset_conversation_button() -> rx.Component:
-    """
-    Render the button to reset the conversation.
-    """
-    return (
-        rx.alert_dialog.root(
-            rx.alert_dialog.trigger(
-                rx.button(
-                    rx.desktop_only(LanguageState.reset_conversation),
-                    rx.mobile_and_tablet(LanguageState.reset_string),
-                    color_scheme="red",
-                    _hover=rx.cond(
-                        ChatState.messages.length() < 2,  # type: ignore
-                        {"cursor": "not-allowed"},
-                        {"cursor": "pointer"},
-                    ),
-                    disabled=rx.cond(
-                        ChatState.messages.length() < 2,  # type: ignore
-                        True,
-                        False,
-                    ),
-                )
+    return destructive_confirm(
+        trigger=rx.button(
+            rx.desktop_only(LanguageState.reset_conversation),
+            rx.mobile_and_tablet(LanguageState.reset_string),
+            color_scheme="pink",
+            _hover=rx.cond(
+                ChatState.messages.length() < 2, # type: ignore
+                {"cursor": "not-allowed"},
+                {"cursor": "pointer"},
             ),
-            rx.alert_dialog.content(
-                rx.alert_dialog.title(LanguageState.reset_conversation),
-                rx.alert_dialog.description(
-                    rx.cond(
-                        ChatState.conversation_is_submitted,
-                        LanguageState.reset_info_message_submitted,
-                        LanguageState.reset_info_message_not_submitted,
-                    )
-                ),
-                rx.hstack(
-                    rx.alert_dialog.cancel(
-                        rx.button(
-                            rx.text(LanguageState.cancel),
-                            color_scheme="red",
-                            _hover={"cursor": "pointer"},
-                        ),
-                    ),
-                    rx.alert_dialog.action(
-                        rx.button(
-                            LanguageState.confirm,
-                            color_scheme="iris",
-                            on_click=ChatState.reset_conversation,
-                            _hover={"cursor": "pointer"},
-                        ),
-                    ),
-                    margin_top="1em",
-                ),
+            disabled=rx.cond(
+                ChatState.messages.length() < 2, # type: ignore
+                True,
+                False,
             ),
         ),
+        title=LanguageState.reset_conversation,
+        description=rx.cond(
+            ChatState.conversation_is_submitted,
+            LanguageState.reset_info_message_submitted,
+            LanguageState.reset_info_message_not_submitted,
+        ),
+        cancel_text=LanguageState.cancel,
+        confirm_text=LanguageState.confirm,
+        on_confirm=ChatState.reset_conversation,
     )
 
 
--- aitutor/pages/finished_view/components.py
+++ aitutor/pages/finished_view/components.py
@@ -1,6 +1,7 @@
 """Components for the Finished View page."""
 
 import reflex as rx
+from aitutor.components import destructive_confirm
 
 from aitutor.pages.finished_view.state import FinishedViewState
 from aitutor.language_state import LanguageState
@@ -8,36 +9,17 @@ from aitutor.language_state import LanguageState
 
 def delete_submission_button() -> rx.Component:
     """
-    Render the button to reset the conversation.
+    Render the button to delete a submission.
     """
-    return rx.alert_dialog.root(
-        rx.alert_dialog.trigger(
-            rx.button(
-                LanguageState.delete_submission,
-                color_scheme="red",
-                _hover={"cursor": "pointer"},
-            )
-        ),
-        rx.alert_dialog.content(
-            rx.alert_dialog.title(LanguageState.delete_submission),
-            rx.alert_dialog.description(LanguageState.delete_submission_info),
-            rx.hstack(
-                rx.alert_dialog.cancel(
-                    rx.button(
-                        rx.text(LanguageState.cancel),
-                        color_scheme="red",
-                        _hover={"cursor": "pointer"},
-                    ),
-                ),
-                rx.alert_dialog.action(
-                    rx.button(
-                        LanguageState.confirm,
-                        color_scheme="iris",
-                        _hover={"cursor": "pointer"},
-                        on_click=FinishedViewState.delete_submisssion,
-                    ),
-                ),
-                margin_top="1em",
-            ),
+    return destructive_confirm(
+        title=LanguageState.delete_submission,
+        description=LanguageState.delete_submission_info,
+        confirm_text=LanguageState.confirm,
+        cancel_text=LanguageState.cancel,
+        on_confirm=FinishedViewState.delete_submisssion,
+        trigger=rx.button(
+            LanguageState.delete_submission,
+            color_scheme="pink",
+            _hover={"cursor": "pointer"},
         ),
     )
