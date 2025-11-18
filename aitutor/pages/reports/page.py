36434678ecd4c7e3750e5f5bf256ad41157e81c9
Andrewtuebingen <andrew.aleid@student.uni-tuebingen.de>
2025-11-18 22:12:48 +0100

    kleine zwischen tests

--- aitutor/pages/reports/page.py
+++ aitutor/pages/reports/page.py
@@ -1,24 +1,22 @@
-"""reports page to report weird chats with Ai."""
+"""Reports page."""
 
 import reflex as rx
-
-
 from aitutor import routes
 from aitutor.models import UserRole
 from aitutor.pages.navbar import with_navbar
 from aitutor.auth.protection import page_require_role_at_least
-
+from .components import reports_table
+from .state import ReportsState
 
 @with_navbar(routes.REPORTS)
-@page_require_role_at_least(UserRole.ADMIN)
+@page_require_role_at_least(UserRole.TUTOR)
 def reports_page() -> rx.Component:
-    """reports page."""
+    """Page for tutors/admins to see all reports."""
     return rx.center(
         rx.vstack(
-            rx.text("Initialize reports page"),
-            spacing="3",
+            reports_table(),
             align="center",
-            justify="center",
+            justify="center"
         ),
         margin_top="2em",
         margin_bottom="2em",
