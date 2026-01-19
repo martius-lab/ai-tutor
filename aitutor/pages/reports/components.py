4c2412059d2fb3bc4ee1d1d618931eac22d08704
Andrewtuebingen <andrew.aleid@student.uni-tuebingen.de>
2026-01-19 22:38:12 +0100

    silent ruff

--- aitutor/pages/reports/components.py
+++ aitutor/pages/reports/components.py
@@ -36,7 +36,7 @@ def show_table_row(table_row: TableRow) -> rx.Component:
         ),
         rx.table.cell(
             rx.cond(
-                table_row.exercise_title == None,
+                table_row.exercise_title == None,  # noqa: E711
                 rx.text(LanguageState.deleted_report_title, color="red"),
                 rx.text(
                     table_row.exercise_title,
