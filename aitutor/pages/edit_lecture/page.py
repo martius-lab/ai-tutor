cf7352421ccbdda6f7fbaaee0f11f56964fdb088
Andrewtuebingen <andrew.aleid@student.uni-tuebingen.de>
2026-05-04 18:38:05 +0200

    remove navbar for lecture

--- aitutor/pages/edit_lecture/page.py
+++ aitutor/pages/edit_lecture/page.py
@@ -7,11 +7,9 @@ from aitutor.auth.protection import page_require_role_or_permission
 from aitutor.models import GlobalPermission
 from aitutor.pages.edit_lecture.components import edit_lecture_form
 from aitutor.pages.navbar import with_navbar
-from aitutor.pages.navbar_lectures import with_lectures_navbar
 
 
 @with_navbar(routes.LECTURES)
-@with_lectures_navbar(routes.EDIT_LECTURE + "/new")
 @page_require_role_or_permission(allowed_permissions=[GlobalPermission.LECTURER])
 def edit_lecture_page() -> rx.Component:
     """Lecture edit/create page."""
--- aitutor/pages/my_lectures/page.py
+++ aitutor/pages/my_lectures/page.py
@@ -7,11 +7,9 @@ from aitutor.auth.protection import page_require_role_or_permission
 from aitutor.models import UserRole
 from aitutor.pages.my_lectures.components import my_lectures_content
 from aitutor.pages.navbar import with_navbar
-from aitutor.pages.navbar_lectures import with_lectures_navbar
 
 
 @with_navbar(routes.LECTURES)
-@with_lectures_navbar(routes.MY_LECTURES)
 @page_require_role_or_permission(required_role=UserRole.STUDENT)
 def my_lectures_page() -> rx.Component:
     """Show the lectures visible to the current user."""
