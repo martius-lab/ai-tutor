20569b706988566d275774a2f46f527cc9989ade
Felix Kloss <felix.kloss@uni-tuebingen.de>
2026-09-09 15:19:23 +0200

    Remove special handling for SQLite
    Since we moved away from SQLite, we don't need the special handling for
    enabling foreign key constraints anymore.

--- rxconfig.py
+++ rxconfig.py
@@ -2,8 +2,6 @@
 
 import reflex as rx
 from pydantic_settings import BaseSettings, SettingsConfigDict
-from sqlalchemy import event
-from sqlalchemy.engine import Engine
 
 
 class PostgresSettings(BaseSettings):
@@ -16,18 +14,6 @@ class PostgresSettings(BaseSettings):
     )
 
     POSTGRES_PASSWORD: str = ""
-
-
-# Enable foreign key constraints for SQLite
-@event.listens_for(Engine, "connect")
-def set_sqlite_pragma(dbapi_conn, connection_record):
-    """Enable foreign key constraints on SQLite connections."""
-    # Check if this is a SQLite connection (sqlite3 or pysqlite2)
-    module_name = dbapi_conn.__class__.__module__
-    if module_name.startswith("sqlite3") or module_name.startswith("pysqlite"):
-        cursor = dbapi_conn.cursor()
-        cursor.execute("PRAGMA foreign_keys=ON")
-        cursor.close()
 
 
 db_settings = PostgresSettings()
